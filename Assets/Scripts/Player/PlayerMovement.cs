a062108aa2e35fafa62c80b1e730444556dfb311
aperro <adrien.perraud@telecom-st-etienne.fr>
2017-10-31 18:11:43 -0400

    Création d'un mini Centre-ville (Adrien)
    J'ai crée un bloc d'immeuble que j'ai copié-collé pour faire 4 patés de maison.
    J'ai ajouté un personnage qui peut se ballader dans le décors mais la caméra heurte les immeubles.
    J'ai mis des collider sur tous les immeubles.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movment;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRaylenght = 100f;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+
+    }
+
+    void Move(float h, float v)
+    {
+        movment.Set(h, 0f, v);
+        movment = movment.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movment);
+
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if(Physics.Raycast(camRay, out floorHit, camRaylenght, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v)
+        {
+            bool walking = h != 0f || v != 0f;
+            anim.SetBool("IsWalking", walking);
+        }
+
+}
